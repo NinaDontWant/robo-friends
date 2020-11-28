0e45f1a32166c2508ec3fa85aeff07d87ebb23af
NinaDontWant <appleheinz@gmail.com>
2020-11-28 02:12:21 -0800

    first cards done

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import './Card.css';
 
-class Card extends React.Component{
-    render() {
+const Card = (props) => {
         return (
-            <div>
-                <img alt='Jane Doe' src='https://robohash.org/rando' />
+            <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+                {/* because of tachyon, ^ abbreviations can be used:
+                tc: text center
+                bg: background
+                dib: the div (what does the b stand for?)
+                br3: the border, likely 3 pixels?
+                pa3: padding of 3
+                ma2: margin of 2
+                grow: an animation
+                bw2: something related to the shadow
+                shadow 5: also related to shadow
+                */}
+            {/* <h1>RoboFriends</h1> */}
+                <img alt='Jane Doe' src={`https://robohash.org/rando${props.id}?200x200`} />
                 <div>
-                    <h2>Jane Doe</h2>
-                    <p>email.address@gmail.com</p>
+                    <h2>{props.name}</h2>
+                    <p>{props.email}</p>
                 </div>
             </div>
         )
     }
-}
 
+// alternatively to having props.id, props.name and props.email, I can insert a line above the return statement like this: **** const {id, name, email}=props; **** then I dont have to write {props.name} etc. where I need it but can instead write {name} --- that's called destructuring. 
 export default Card; 
---
+++ src/robots.js
@@ -0,0 +1,34 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Anton Antonovic',
+        username: 'Tonio',
+        email: 'silly-me-1991@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Berta Bertramova',
+        username: 'Beri',
+        email: 'dontmessage@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Cäsar Kaiser',
+        username: 'Hoheit',
+        email: 'spectacular.me@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Dora Donnerstag',
+        username: 'Dodo',
+        email: 'me-1982-me@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Emil Milanovic',
+        username: 'Milo',
+        email: 'silly-address@gmail.com'
+    },
+]
+
+export default robots; 
