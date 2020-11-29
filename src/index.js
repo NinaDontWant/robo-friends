5cdd7855be1bc418a4edc8b04751457c37e9d239
NinaDontWant <appleheinz@gmail.com>
2020-11-29 00:25:17 -0800

    added file CardList

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 // import Hello from './Hello';
-import Card from './Card';
+// import Card from './Card';
+import CardList from './CardList.js';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'; 
 import { robots } from './robots';
@@ -16,12 +17,11 @@ import { robots } from './robots';
 //   // <h1>Hello World</h1>,
 //   // document.getElementById('root')
 //   );
+
+
 ReactDOM.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-  </div>
+
+  <CardList robots={robots}/>
   , document.getElementById('root'));
   
 // If you want to start measuring performance in your app, pass a function
