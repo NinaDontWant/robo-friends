74eefcf3d0ddc5c4b19a809d1f1a6274d2749f9e
NinaDontWant <appleheinz@gmail.com>
2020-12-09 23:46:05 -0800

    Search
    new function for search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 //import './App.css';
 import { robots } from './robots';
 import CardList from './CardList.js';
 import SearchComponent from './SearchComponent';
 
- 
-class App extends React.Component {
+// robots (or is it CardList?) and SearchComponent are both children of the same parent. what state should this have so that the child can communicate with its parent aka the SearchComponent with its parent who will then talk to the other child, robots, and pass on information
+
+
+// because I wrote "React, {Component}" in the imports (the comma is important), I don't have to write "class App extends React.Component", I can leave out the "React." part because it's already clear.
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="App tc ">
@@ -14,8 +29,8 @@ class App extends React.Component {
             <h1 className="App-title">Robo friends</h1>
           </p>
         </header>
-        <SearchComponent />
-        <CardList robots={robots}/>
+        <SearchComponent searchChange={this.onSearchChange} />
+        <CardList robots={this.state.robots}/>
 
       </div>
     );
--- src/SearchComponent.js
+++ src/SearchComponent.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-class SearchComponent extends React.Component {
-    render() {
+const SearchComponent = ({searchChange}) => {
+    
         return (
             <div className ='pa2'>
                 <input
                     className='pa3 b--green bg-lightest-blue'
                     type='search'
                     placeholder='search robots'
+                    onChange={searchChange}
             />
             </div>    
         )
     }
-}
+
 
 //remember tachyon; pa2 padding 2, b--green border green, bg-xxx backgroundcolour
 
